37fdb9fe3c8c9fef46104617019bbc8edcb7d8a8
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2021-06-27 19:38:41 +0700

    s
    s

---
+++ public/js/main.js
@@ -0,0 +1,282 @@
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "page/main.html",
+        controller : "mainCtrl"
+    })
+    .when("/jackpot", {
+        templateUrl : "page/jackpot.html"
+    })
+    .when("/my-bag", {
+        templateUrl : "page/my-bag.html"
+    })
+    .when("/bonus", {
+        templateUrl : "page/bonus.html"
+    })
+    .when("/my-profile", {
+        templateUrl : "page/my-profile.html"
+    })
+    .otherwise({
+      template : "<p>Sorry when don't support yet</p>"
+  });;
+});
+
+app.controller('controller', function($scope, $http) {
+   $scope.websiteName = "BET MARKET";
+   $scope.menu = [];
+   $scope.user = {
+      username: "test",
+      data: "login fail"
+   };
+   console.log($scope.menu);
+   var vm = this;
+   vm.changeActiveMenu = function(url) {
+      for (let i of $scope.menu) {
+         if (i.url === url) {
+            i.active = "active";
+         }else{
+            i.active = "";
+         }
+      }
+   };
+
+   $scope.liveID = readCookie('liveID');
+
+   $http.get("/user/" + readCookie('liveID')).then(function (response) {
+      console.log(response.data);
+      $scope.user = response.data;
+   });
+   
+   vm.login = function() {
+      $('#loginModal').modal('hide');
+      $('#loadingModal').modal('show');
+      var posting = $.post( "/login",
+       { email: $('#emailLogin').val(), password: $('#passwordLogin').val() } );
+      posting.done(function( data ) {
+         $scope.user = JSON.parse(data);
+         if ($scope.user.username){
+            document.cookie = "liveID=" + $scope.user.hash;
+            $scope.liveID = readCookie('liveID');
+            $scope.$apply();
+         }
+         $('#loadingModal').modal('hide');
+      });
+   };
+   vm.logout = function() {
+      deleteAllCookies();
+      $scope.user = {};
+      $scope.liveID = null;
+   }
+   $scope.bet = {
+      teamBet: 0,
+      rel: '',
+      valueBet: 0
+   };
+   vm.bet = function(idGame) {
+      console.log(idGame);
+      for (let i of $scope.lol) {
+         if (idGame === i.rel) {
+            $scope.bet = i;
+            console.log($scope.bet);
+             $('#betModal').modal('show');
+            break;
+         }
+      }
+   }
+   vm.change = function(idGame) {
+      console.log(idGame);
+      // for (let i of $scope.lol) {
+      //    if (idGame === i.rel) {
+      //       $scope.bet = i;
+      //       console.log($scope.bet);
+      //        $('#betModal').modal('show');
+      //       break;
+      //    }
+      // }
+   }
+   vm.cancel = function(idGame) {
+      $('#loginModal').modal('show');
+   }
+   $http.get("/next-match/lol").then(function (response) {
+      $scope.lol = response.data;
+   });
+   $http.get("/menu").then(function (response) {
+      $scope.menu = response.data;
+   });
+   $http.get("/leaguePrize").then(function (response) {
+      $scope.names = response.data;
+   });
+
+   $scope.bet.valueBet = 0;
+   vm.totalValueBet = function() {
+      var totalValue = 0.00;
+      for (let i of $scope.inventory) {
+         if (i.betted) {
+            totalValue += i.value;
+         }
+      }
+      return totalValue;
+   }
+   vm.checkBet = function () {
+      console.log('checkBet');
+      $scope.bet.valueBet = vm.totalValueBet();
+   }
+   vm.totalValue = function() {
+      var totalValue = 0.00;
+      for (let i of $scope.inventory) {
+         if ((i.type === $scope.statusBag) && (!i.betted)) {
+            totalValue += i.value;
+         }
+      }
+      return totalValue;
+   }
+   vm.totalItem = function() {
+      var totalItem = 0;
+      for (let i of $scope.inventory) {
+         if ((i.type === $scope.statusBag) && (!i.betted)) {
+            totalItem++;
+         }
+      }
+      return totalItem;
+   }
+
+   $scope.setBackground = function (item) {
+      return item.style;
+   }
+   $scope.urlItem = "https://steamcommunity-a.akamaihd.net/economy/image/W_I_5GLm4wPcv9jJQ7z7tz_l_0sEIYUhRfbF4arNQkgGQGKd3kMuVpMgCwRZrhuYeVbf2uNDa_HZCjEuH5nvSUryOaKDx1uiU-9Qf9V1NmFX2dro004bBiXRVOUUCNUitZmS1g26WADFfDduw4QBgKXM1M-HCPPSrAynLlT3xxqopQ";
+   $scope.value = 10;
+   $scope.betInventory = {
+      dota2: [],
+      csgo: []
+   }
+   $scope.status = "none";
+   $scope.statusBag = "dota2";
+   vm.inventoryByType = function() {
+      var array = [];
+      for (let i of $scope.inventory) {
+         if ((i.type === $scope.statusBag) && (!i.betted)) {
+            array.push(i);
+         }
+      }
+      return array;
+   }
+   vm.betInventoryByType = function(name) {
+      var array = [];
+      for (let i of $scope.inventory) {
+         if ((i.type === name) && (i.betted)) {
+            array.push(i);
+         }
+      }
+      return array;
+   }
+   vm.betItem = function(id) {
+      for (let i of $scope.inventory) {
+         i.betted = true;
+      }
+   }
+   vm.betGame = function(team, obj) {
+      console.log(team, obj);
+      var array = [];
+      for (let i of $scope.inventory) {
+         if (i.betted){
+            array.push(i);
+         }
+      }
+      var objBet = {
+         teamBet: team,
+         rel: obj.rel,
+         items: array
+      }
+      console.log(objBet);
+   }
+   $scope.inventory = [
+      {
+         _id: "1",
+         user_id: "1",
+         value: 1.02,
+         name: "name 1.02",
+         type: "dota2",
+         style: {
+            "background-image": "url("+ $scope.urlItem + ")"
+         }
+      },
+      {
+         _id: "2",
+         user_id: "1",
+         value: 2.02,
+         name: "name 2.02",
+         type: "dota2",
+         style: {
+            "background-image": "url("+ $scope.urlItem + ")"
+         }
+      },
+      {
+         _id: "3",
+         user_id: "1",
+         value: 5.00,
+         name: "name 5.00",
+         type: "csgo",
+         style: {
+            "background-image": "url("+ $scope.urlItem + ")"
+         }
+      },
+      {
+         _id: "4",
+         user_id: "1",
+         value: 10.02,
+         name: "name 10.02",
+         type: "csgo",
+         style: {
+            "background-image": "url("+ $scope.urlItem + ")"
+         }
+      },
+      {
+         _id: "5",
+         user_id: "1",
+         value: 20.00,
+         name: "name 20",
+         type: "dota2",
+         style: {
+            "background-image": "url("+ $scope.urlItem + ")"
+         }
+      },
+      {
+         _id: "6",
+         user_id: "1",
+         value: 20.00,
+         name: "name 20",
+         type: "csgo",
+         style: {
+            "background-image": "url("+ $scope.urlItem + ")"
+         }
+      }
+   ];
+});
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function deleteAllCookies() {
+   var cookies = document.cookie.split(";");
+
+   for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+   }
+}
+
+app.controller('mainCtrl', function($scope, $http) {
+   console.log("Main HTML");
+});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://admin:bd11112222@@rabbitcluster.jjait.mongodb.net/";
 const dbName = 'myFirstDatabase';
@@ -6,15 +7,12 @@ const dbName = 'myFirstDatabase';
 const express = require('express');
 const { Server } = require('ws');
 const cors = require('cors');
+const https = require('https');
 const bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var parse = require('node-html-parser');
 
 const PORT = process.env.PORT || 3000;
-// const INDEX = 'public/index.html';
-
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-console.clear();
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -26,12 +24,12 @@ app.options('*', cors());
 var server = app.listen(PORT, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
+    log("Example app listening at http://%s:%s", host, port);
 });
 
 const wss = new Server({ server });
 
-var CLIENTS=[];
+var CLIENTS = [];
 wss.on('connection', function connection(ws) {
     CLIENTS.push(ws);
     ws.on('message', function incoming(message) {
@@ -40,7 +38,7 @@ wss.on('connection', function connection(ws) {
 });
 
 function sendAll (message) {
-    for (var i=0; i<CLIENTS.length; i++) {
+    for (var i = 0; i < CLIENTS.length; i++) {
         CLIENTS[i].send(message);
     }
 }
@@ -59,10 +57,283 @@ MongoClient.connect(url, function(err, db) {
         var data = [
             {url: "", name: "Predictions", active: "active"},
             {url: "jackpot", name: "Jackpot", active: ""},
-            {url: "product", name: "My Bag", active: ""},
-            {url: "demo", name: "Bonus", active: ""},
-            {url: "contact", name: "My Profile", active: ""}
+            {url: "my-bag", name: "My Bag", active: ""},
+            {url: "bonus", name: "Bonus", active: ""},
+            {url: "my-profile", name: "My Profile", active: ""}
         ];
         res.end( JSON.stringify(data));
     });
+
+    app.post('/sign-up', function (req, res, next) {
+        var body = req.body;
+        console.log("Có người đăng ký" ,body);
+        var username = body.username;
+        var email = body.email;
+        var password = body.password;
+        var obj = {
+            time: Date.now(),
+            username: username,
+            email: email,
+            password: password,
+            hash: generateCodeActive()
+        }
+        findObj(dbo, "signup", {email: email}, function(data) {
+            if (data.length === 0) {
+                insertObj(dbo, "signup", [obj], function(data2) {
+                    if (data2.data === 1) {
+                        sendActiveMail(obj.email, obj.hash, function(data3) {
+                            res.send("Please user another email. We can't send to your email.");
+                        });
+                    }
+                    res.send("Check your email: " + email + " to activate your account.");
+                });
+            }else{
+                res.end("Email has been used.");
+            }
+        });
+    });
+
+    app.get('/verification-account/:pass', function (req, res) {
+        var pass = req.params.pass;
+        log(pass);
+        findObj(dbo, "signup", {hash: pass}, function(data) {
+            if (data.length === 1) {
+                var obj = data[0];
+                delete obj['_id'];
+                insertObj(dbo, "users", [obj], log);
+                deleteObj(dbo, "signup", {hash: pass}, log);
+                res.send("Your account is activated.");
+            }
+        });
+    });
+
+    app.post('/login', function (req, res, next) {
+        var body = req.body;
+        log("Có người đăng nhập" , body);
+        var obj = {
+            email: body.email,
+            password: body.password
+        }
+        findObj(dbo, "users", obj, function(data) {
+            if (data.length === 1) {
+                var rs = data[0];
+                delete rs["password"];
+                res.end( JSON.stringify(rs));
+            }else{
+                res.end( JSON.stringify({data: "login fail"}));
+            }
+        });
+    });
+
+    app.get('/user/:hash', function (req, res) {
+        var hash = req.params.hash;
+        log("Có người đăng nhập", hash);
+        findObj(dbo, "users", {hash: hash}, function(data) {
+            if (data.length === 1) {
+                var rs = data[0];
+                delete rs["password"];
+                res.end( JSON.stringify(rs));
+            }else{
+                res.end( JSON.stringify({data: "login fail"}));
+            }
+        });
+    });
+
+    app.get('/leaguePrize/:type', function (req, res) {
+        var leaguePrize = [];
+        res.end( JSON.stringify(leaguePrize));
+    });
+
+    app.get('/next-match/:type', function (req, res) {
+        getData(req.params.type, function(data) {
+            res.end( JSON.stringify(data));
+        });
+    });
 });
+
+function checkCollectionExist(dbo, collName, callback) {
+    dbo.listCollections({name: collName}).next(function(err, collinfo) {
+        callback(collinfo);
+    });
+}
+
+function createCollection(dbo, collName, callback) {
+    dbo.createCollection(collName, function(err, res) {
+        if (err) throw err;
+        log("Collection created!");
+        callback(res);
+    });
+}
+
+function insertObj(dbo, collName, array, callback) {
+    dbo.collection(collName).insertMany(array, function(err, res) {
+        if (err) throw err;
+        log(res.insertedCount + " document(s) inserted");
+        callback({data : res.insertedCount});
+    });
+}
+
+function deleteObj(dbo, collName, obj, callback) {
+    dbo.collection(collName).deleteMany(obj, function(err, res) {
+        if (err) throw err;
+        log(res.result.n + " document(s) deleted");
+        callback({data : res.result.n});
+    });
+}
+
+function updateObj(dbo, collName, obj, newObj, callback) {
+    var newvalues = { $set: newObj };
+    dbo.collection(collName).updateMany(obj, newvalues, function(err, res) {
+        if (err) throw err;
+        log(res.result.nModified + " document(s) updated");
+        callback({data : res.result.nModified});
+    });
+}
+
+function findObj(dbo, collName, obj, callback) {
+    dbo.collection(collName).find(obj).toArray(function(err, result) {
+        // after find .limit(5)
+        if (err) throw err;
+        callback(result);
+    });
+}
+
+function log(message) {
+    if (true) console.log(message);
+}
+
+function generateCodeActive() {
+    var length = 32,
+        charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        result = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        result += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return result;
+}
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'canhnt1204@gmail.com',
+    pass: '1204thanhcanh1994'
+  }
+});
+
+function sendActiveMail(email, code, callback) {
+    var mailOptions = {
+      from: 'canhnt1204@gmail.com',
+      to: email,
+      subject: 'Your Bet Market Verification Code',
+      text: 'Your activation link : ' + apiUrl + '/verification-account/' + code
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        callback(error);
+      } else {
+        log('ĐÃ GỬI 1 MAIL XÁC NHẬN: ' + info.response);
+      }
+    });
+}
+
+async function chttps(method, url, data) {
+  const dataString = JSON.stringify(data)
+
+  const options = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length,
+    },
+    timeout: 5000, // in ms
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(url, options, (res) => {
+      if (res.statusCode < 200 || res.statusCode > 299) {
+        return reject(new Error(`HTTP status code ${res.statusCode}`))
+      }
+
+      const body = []
+      res.on('data', (chunk) => body.push(chunk))
+      res.on('end', () => {
+        const resString = Buffer.concat(body).toString()
+        resolve(resString)
+      })
+    })
+
+    req.on('error', (err) => {
+      reject(err)
+    })
+
+    req.on('timeout', () => {
+      req.destroy()
+      reject(new Error('Request time out'))
+    })
+
+    req.write(dataString)
+    req.end()
+  })
+}
+
+async function getData(name, callback){
+   // console.clear();
+   // console.log(Date.now() + " Start");
+   const res = await chttps('GET', 'https://en.game-tournaments.com/' + name, "");
+   const root = parse.parse(res);
+   //console.log(root.querySelector('#block_matches_current'));
+   var matchs_past = root.querySelector('#block_matches_current').querySelector('table').childNodes;
+   var array = [];
+   for (let i of  matchs_past) {
+      if (i.rawTagName === 'tr'){
+         var obj = {};
+         obj.rel = i.rawAttrs.substring(5, i.rawAttrs.indexOf(' class="mlive') -1);
+         var str = i.childNodes[3].childNodes[1].rawAttrs;
+         obj.link = str.substring(str.indexOf('href=')+6, str.indexOf(' title=') - 1);
+         obj.title = str.substring(str.indexOf('tle=') + 5, str.indexOf(' class=') - 1);
+
+         obj.time = i.childNodes[5].childNodes[3].childNodes[1].rawText;
+
+         obj.team1 = i.childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes[1].rawText;
+         obj.teamName1 = "";
+         obj.betPer1 = parseFloat(i.childNodes[3].childNodes[1].childNodes[3].childNodes[1].rawText.substring(1,5));
+         obj.betPer1 = isNaN(obj.betPer1) ? 0 : obj.betPer1;
+         
+         obj.team2 = i.childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[1].rawText;
+         obj.teamName2 = "";
+         obj.betPer2 = parseFloat(i.childNodes[3].childNodes[1].childNodes[3].childNodes[5].rawText.substring(1,5));
+         obj.betPer2 = isNaN(obj.betPer2) ? 0 : obj.betPer2;
+
+         str = i.childNodes[7].childNodes[1].rawAttrs;
+         obj.leagueLink = str.substring(str.indexOf('href=')+6, str.indexOf(' title=') - 1);
+         obj.leagueName = str.substring(str.indexOf('tle=') + 5, str.length - 1);
+
+         obj.teamName1 = obj.link.split(obj.leagueLink + '/')[1].split('-vs-')[0];
+         obj.teamName2 = obj.link.split(obj.leagueLink + '/')[1].split('-vs-')[1].split('-' + obj.rel)[0];
+         str = obj.leagueLink.split("/");
+         obj.typeGame = str[3];
+         obj.leagueLink = '/' + str[1] + '/' + str[2];
+         array.push(obj);
+      }
+   }
+   callback(array);
+
+   var prize_pool;
+   if (name === 'lol'){
+      prize_pool = root.querySelector('.col-right').querySelector('table').childNodes[1].childNodes;
+   }else{
+      prize_pool = root.querySelector('.col-right').childNodes[3].querySelector('table').childNodes[1].childNodes;
+   }
+   var array_prize_pool = [];
+   for (let i of  prize_pool) {
+      if (i.rawTagName === 'tr'){
+         var obj = {}
+         obj.leagueName = i.childNodes[1].childNodes[0].childNodes[0].rawText;
+         obj.leagueLink = i.childNodes[1].childNodes[0].rawAttrs.substring(6).replace('"','');
+         obj.leaguePrize = parseInt(i.childNodes[3].childNodes[1].rawText.split(",").join(""));
+         array_prize_pool.push(obj);
+      }
+   }
+   // console.log(array_prize_pool);
+}
