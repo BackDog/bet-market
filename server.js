72650e59d25fa896b411773d2c961fce400e51a6
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2021-06-27 16:51:10 +0700

    s
    s

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://admin:bd11112222@@rabbitcluster.jjait.mongodb.net/";
 
 const express = require('express');
 const { Server } = require('ws');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = 'public/index.html';
+// const INDEX = 'public/index.html';
 
 // const server = express()
 //   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -12,6 +16,10 @@ const INDEX = 'public/index.html';
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.options('*', cors());
 
 var server = app.listen(PORT, function () {
     var host = server.address().address;
@@ -34,3 +42,19 @@ function sendAll (message) {
         CLIENTS[i].send(message);
     }
 }
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(dbName);
+
+    app.get('/menu', function (req, res) {
+        var data = [
+            {url: "", name: "Predictions", active: "active"},
+            {url: "jackpot", name: "Jackpot", active: ""},
+            {url: "product", name: "My Bag", active: ""},
+            {url: "demo", name: "Bonus", active: ""},
+            {url: "contact", name: "My Profile", active: ""}
+        ];
+        res.end( JSON.stringify(data));
+    });
+});
